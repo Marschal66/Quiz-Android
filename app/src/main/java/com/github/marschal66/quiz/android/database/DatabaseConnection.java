f72bbb0000cc529fd471915121be3dab90ebb93f
Marschal66 <marschal66@web.de>
2023-05-08 18:46:32 +0200

    Added KSP

--- app/src/main/java/com/github/marschal66/quiz/android/database/DatabaseConnection.java
+++ app/src/main/java/com/github/marschal66/quiz/android/database/DatabaseConnection.java
@@ -1,7 +1,5 @@
 package com.github.marschal66.quiz.android.database;
 
-import java.sql.*;
-
 public class DatabaseConnection {
 
     public DatabaseConnection() {
---
+++ app/src/main/java/com/github/marschal66/quiz/android/database/data/Question.java
@@ -0,0 +1,7 @@
+package com.github.marschal66.quiz.android.database.data;
+
+
+public class Question {
+    private int id;
+    private String question;
+}
